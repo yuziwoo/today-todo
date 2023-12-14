235aff12c79912cd44ea7f54b9fbfa269949eac2
yuziwoo <yuziwoo@naver.com>
2023-12-14 16:06:06 +0900

    feat: add lotties Complete effect

---
+++ src/components/effect/Complete.tsx
@@ -0,0 +1,17 @@
+import { Player } from '@lottiefiles/react-lottie-player';
+
+const Complete = () => {
+  return (
+    <div>
+      <Player
+        autoplay
+        loop
+        src="https://lottie.host/c07a75ea-9210-4683-97b8-3a48cf3c5f42/MXdfhSaAT8.json"
+        style={{ height: '300px', width: '300px' }}
+      >
+      </Player>
+    </div>
+  )
+}
+
+export default Complete;
