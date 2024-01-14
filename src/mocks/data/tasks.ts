765dfa79bb8e8e57566b15be1964447a0dcb466b
yuziwoo <yuziwoo@naver.com>
2024-01-14 15:04:54 +0900

    docs: todo mock data 수정

--- src/mocks/data/tasks.ts
+++ src/mocks/data/tasks.ts
@@ -5,18 +5,18 @@ export const tasks: Tasks = {
   tasks: [
     {
       id: 1000,
-      year: 2024,
-      month: 0,
-      day: 30,
-      works: '공부하기',
+      year: 2023,
+      month: 11,
+      day: 31,
+      works: '2023년 마지막 일기 쓰기',
       complete: false,
     },
     {
       id: 1005,
-      year: 2024,
-      month: 0,
-      day: 30,
-      works: '공부하기2',
+      year: 2023,
+      month: 11,
+      day: 31,
+      works: '2024년 계획 세우기',
       complete: true,
     },
   ],
@@ -47,7 +47,7 @@ export const tasks: Tasks = {
         start: 20240019,
         repeat: [2, 4],
         end: 20240030,
-        works: '도서관 가기',
+        works: '매 주 화,목 도서관 가기',
         complete: [
           {
             year: 2024,
